c5991b88aff5b2d96fcc149c7cee8d29b04abc31
sdesyllas <sdesyllas@hotmail.com>
2015-03-15 12:36:03 +0200

    fixed latitude south pole / north pole min max values

--- seed.rb
+++ seed.rb
@@ -10,18 +10,17 @@ people.drop
 people.create_index("userId")
 people.create_index(:loc => Mongo::GEO2D)
 # create 100 random users
-100.times { |i| 
+100.times { |i|
 	doc = {
-		"userId" => i, 
-		"userName" => "TestUser", 
-		"count" => 1, 
+		"userId" => i,
+		"userName" => "TestUser",
+		"count" => 1,
 		"friend_ids" => [i-2, i-1, i+1, i+2],
 		#Random Coordinates
-		"loc" => [Random.rand(-180.000000..180.000000), Random.rand(-180.000000..180.000000)] 
+		"loc" => [Random.rand(-90.000000..90.000000), Random.rand(-180.000000..180.000000)] 
 	}
 	id = people.insert(doc)
 	puts "Inserted : #{doc}"
 }
 total = people.count
 puts "Inserted #{total} users and scattered at random coordinates"
-
